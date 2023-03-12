c413f2d7f341423315b45a442b1f0f7940cddf92
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-03-11 23:01:21 -0500

    Rename and fix url

---
+++ Casks/lumen-synth.rb
@@ -0,0 +1,22 @@
+cask "lumen-synth" do
+  version "1.1.2"
+  sha256 :no_check
+
+  url "https://updates.paracosm.us/lumen/stable/download"
+  name "Lumen"
+  desc "Analog-Style Software Video Synthesizer"
+  homepage "https://lumen-app.com/"
+
+  depends_on macos: ">= :mojave"
+
+  app "Lumen.app"
+
+  zap trash: [
+    "/Users/Shared/Lumen",
+    "~/Library/Application Support/Lumen",
+    "~/Library/Application Support/com.Paracosm.Lumen",
+    "~/Library/Cache/com.Paracosm.Lumen",
+    "~/Library/HTTPStorage/com.Paracosm.Lumen",
+    "~/Library/Preferences/com.Paracosm.Lumen.plist",
+  ]
+end
