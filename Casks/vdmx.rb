d6651f128bad95c199fd7135635092c798105984
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-03-11 20:11:35 -0500

    Update cask script after brew checks

--- Casks/vdmx.rb
+++ Casks/vdmx.rb
@@ -1,27 +1,26 @@
 cask "vdmx" do
   version "b8.8.0.5"
-  sha256 "9508e32b2c69a8eb48438f0307b4db1dfb41ca1ff6234386bbb1c85a4a8498c3"
+  sha256 :no_check
 
   url "https://vidvox.net/download/vdmx_demo"
   name "VDMX5"
   desc "Professional VJ Software"
-  homepage "https://vidvox.net"
+  homepage "https://vidvox.net/"
 
   depends_on macos: ">= :high_sierra"
-
-  pkg "VDMX5 Installer.pkg"
-  pkg "Extras/v002 QC Software.pkg"
 
   app "Extras/OSCQuery Browser.app", target: "/Applications/VDMX Extras/OSCQuery Browser.app"
   app "Extras/OSCQuery Helper.app", target: "/Applications/VDMX Extras/OSCQuery Helper.app"
   app "Extras/MIDI OSCQuery Helper.app", target: "/Applications/VDMX Extras/MIDI OSCQuery Helper.app"
   app "Extras/AVF Batch Converter.app", target: "/Applications/VDMX Extras/AVF Batch Converter.app"
+  pkg "VDMX5 Installer.pkg"
+  pkg "Extras/v002 QC Software.pkg"
 
   uninstall pkgutil: [
     "com.vidvox.pkg.VDMX5Installer",
     "com.Vidvox.pkg.VidvoxISFresources",
     "com.v002.pkg.VDMXQCFX",
-    "com.v002.pkg.QCPlugins"
+    "com.v002.pkg.QCPlugins",
   ]
 
   zap trash: [
@@ -31,7 +30,7 @@ cask "vdmx" do
     "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.vidvox.vdmx5.sfl2",
     "~/Library/Caches/QCCompositionRepository-com.vidvox.VDMX5.cache",
     "~/Library/Preferences/com.vidvox.VDMX5.plist",
-    "/Applications/VDMX Extras"
+    "/Applications/VDMX Extras",
   ]
 end
 
