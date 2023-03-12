8fce389739fa252861f4652c1c54f41d524e454a
virgilsisoe <28490646+sisoe24@users.noreply.github.com>
2023-03-12 13:23:18 -0400

    Update casks style

--- Casks/syphon-simple.rb
+++ Casks/syphon-simple.rb
@@ -1,18 +1,17 @@
 cask "syphon-simple" do
-    version "5.0.0"
-    sha256 :no_check
-  
-    url "https://github.com/Syphon/Simple/releases/download/5/Syphon.Simple.Apps.zip"
-    name "Syphon Simple"
-    desc "Realtime live video sharing application"
-    homepage "https://github.com/Syphon/Simple"
-  
-    app "Syphon Simple Apps/Simple Client.app"
-    app "Syphon Simple Apps/Simple Server.app"
-  
-    zap trash: [
-      "~/Library/Preferences/info.v002.syphon.Simple-Client.plist",
-      "~/Library/Preferences/info.v002.syphon.Simple-Server.plist",
-    ]
-  end
-  
+  version "5.0.0"
+  sha256 :no_check
+
+  url "https://github.com/Syphon/Simple/releases/download/5/Syphon.Simple.Apps.zip"
+  name "Syphon Simple"
+  desc "Realtime live video sharing application"
+  homepage "https://github.com/Syphon/Simple"
+
+  app "Syphon Simple Apps/Simple Client.app"
+  app "Syphon Simple Apps/Simple Server.app"
+
+  zap trash: [
+    "~/Library/Preferences/info.v002.syphon.Simple-Client.plist",
+    "~/Library/Preferences/info.v002.syphon.Simple-Server.plist",
+  ]
+end
